ab05d0cfa71b1f6366c5e1e53312370dc7b8da2e
David Zeng <davidlzeng@gmail.com>
2014-09-23 00:30:13 -0700

    MMA functions and tests

--- src/MMA.jl
+++ src/MMA.jl
@@ -1,5 +1,25 @@
 module MMA
 
-# package code goes here
+export std, var, dist, rms, angle
+
+function std(x)
+  return vecnorm(x - mean(x))/sqrt(length(x))
+end
+
+function var(x)
+  return sum((x - mean(x)).^2)/length(x)
+end
+
+function dist(x, y)
+  return vecnorm(x - y)
+end
+
+function rms(x)
+  return vecnorm(x)/sqrt(length(x))
+end
+
+function angle(x, y)
+  return acos(dot(x, y)/(vecnorm(x) * vecnorm(y)))
+end
 
 end # module
