e44ae496d76e925e778cf70b48298aaaf4e3ad0a
kgo <keegango@stanford.edu>
2015-09-03 13:09:19 -0700

    changed MMA to store paths for plot.jl and image.jl to include later

--- src/MMA.jl
+++ src/MMA.jl
@@ -3,6 +3,9 @@ export MMA_load_plotting, MMA_load_images
 
 include("math.jl")
 
+mma_plot_path = abspath("plot.jl")
+mma_image_path = abspath("plot.jl")
+
 function __init__()
 	println("loaded MMA")
 	println("\tuse MMA_load_plotting() to load plotting")
@@ -11,13 +14,13 @@ end
 
 function MMA_load_plotting()
 	println("loading MMA plotting (this can take a while)...")
-	include("plot.jl")
+	include(mma_plot_path)
 	println("done")
 end
 
 function MMA_load_images()
 	println("loading MMA image handling (this can take a while)...")
-	include("images.jl")
+	include(mma_image_path)
 	println("done")
 end
 
